c04647f87e284056fd8f7422cf80f80f69b24ff6
keyvir <kimyu711@naver.com>
2019-09-23 15:57:14 +0900

    page route 업데이트

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,38 +4,46 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Page;
+use App\PageHandler;
+use App\UserHandler;
 use App\Http\Middleware\Cors;
 
 class PageController extends Controller 
 {
-    public function get()
+	public function __construct()
+	{
+		$this->middleware(Cors::class);
+    }
+    
+    public function get(PageHandler $pageHandler, $id)
     {
-        
+        return $pageHandler->get($id);
     }
 
     public function getList()
     {
-        
+        return $pageHandler->getList();
     }
 
     public function getMyList()
     {
-
+        return $pageHandler->getMyList();
     }
 
-    public function store()
+    public function store(Request $request)
     {
-
+        return $pageHandler->store($request->only('content'));
     }
 
-    public function update()
+    public function update(Page $page, Request $request)
     {
-
+        return $pageHandler->update($page, $request->all());
     }
 
-    public function remove()
+    public function remove(Page $page)
     {
-        
+        return $pageHandler->remove($page);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,12 @@ Route::get('/', function () {
 
 Route::post('login','ApiController@login');
 Route::post('register','ApiController@register');
+
+Route::prefix('page')->group(function(){
+    Route::get('{id}','PageController@get');
+    Route::get('list','PageController@getList');
+    Route::get('my-list','PageController@getMyList');
+    Route::post('store','PageController@store');
+    Route::post('update/{page}','PageController@update');
+    Route::get('remove/{page}','PageController@remove');
+});

80e8603ac44140207a59d438b18c5c27fc5fea63
keyvir <kimyu711@naver.com>
2019-09-23 17:42:01 +0900

    생성/수정일 변경

--- app/Page.php
+++ app/Page.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Page extends Model
 {
     protected $guarded = [];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return \Carbon\Carbon::parse($value)->locale('ko')->diffForHumans();
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return \Carbon\Carbon::parse($value)->locale('ko')->diffForHumans();
+    }
 }
